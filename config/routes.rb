62e9f6973b39ec0e35114780e98cf4bb86ba7869
Joon Ho Shin <joonhoshin2021@gmail.com>
2021-11-16 15:29:40 -0500

    update#2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :watches
+  resources :watches, except: :index do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings do
+    resources :reviews, only: [:new, :create]
+  end
 end
