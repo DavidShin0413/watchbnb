19257396a60417363c81dcec9d061a4bcc11b37c
Alexander Chkliar <alex.chkliar@gmail.com>
2021-11-18 11:55:02 -0500

    accept-reject added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,9 @@ class Booking < ApplicationRecord
   belongs_to :watch
 
   validates :start_date, :end_date, presence: true
+  enum status: {
+    pending: 0,
+    accepted: 1,
+    rejected: 2
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_020333) do
+ActiveRecord::Schema.define(version: 2021_11_18_154405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_11_18_020333) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.bigint "watch_id", null: false
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["watch_id"], name: "index_bookings_on_watch_id"
   end
