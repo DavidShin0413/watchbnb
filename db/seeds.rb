474cd29f68727cd26a621774651024009e18429b
Alexander Chkliar <alex.chkliar@gmail.com>
2021-11-18 18:12:59 -0500

    Latest version before end of day, small seed and layout changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,4 +30,18 @@ booking10 = Booking.create(user_id: alex.id, watch_id: ap.id, start_date: (Date.
 booking11 = Booking.create(user_id: alex.id, watch_id: ap.id, start_date: (Date.current + 21), end_date: (Date.current + 22), status: 1)
 booking12 = Booking.create(user_id: alex.id, watch_id: ap.id, start_date: (Date.current + 23), end_date: (Date.current + 24), status: 2)
 
+puts "Creating reviews..."
+dummy_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.\
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown\
+              printer took a galley of type and scrambled it to make a type specimen book. It has survived\
+              not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.\
+              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,\
+              and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+review1 = Review.create(comment: dummy_text, rating: 5, watch_id: rolex.id)
+review2 = Review.create(comment: dummy_text, rating: 4, watch_id: rolex.id)
+review3 = Review.create(comment: dummy_text, rating: 3, watch_id: rolex.id)
+review4 = Review.create(comment: dummy_text, rating: 2, watch_id: rolex.id)
+review5 = Review.create(comment: dummy_text, rating: 1, watch_id: rolex.id)
+
 puts "Done seeding!"
