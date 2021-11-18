262fa17224bd6f634ae43825db7b307ebad471d0
Leonardo Van Schaik <leonardo.vanschaik@gmail.com>
2021-11-18 14:49:32 -0500

    Added search bar function to watches page and removed it from navbar

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -5,4 +5,11 @@ class Watch < ApplicationRecord
 
   validates :name, :price, :location, :img_url, presence: true
   validates :style, inclusion: { in: %w[Dress Diver Pilot Field] }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_style,
+    against: [ :name, :style ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
