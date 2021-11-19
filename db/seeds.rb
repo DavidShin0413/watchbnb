665e82fd744b03c95e010b12322efc3b84d66fdc
Joon Ho Shin <joonhoshin2021@gmail.com>
2021-11-19 16:11:21 -0500

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,20 +15,17 @@ casio = Watch.create(user_id: alex.id, name: "Casio", style: "Field", location: 
 longines = Watch.create(user_id: alex.id, name: "Longines", style: "Dress", location: "Chicoutimi", price: 75, img_url: "https://www.watchtime.com/wp-content/uploads/2016/10/conquest_550_FI-1.jpg")
 iwc = Watch.create(user_id: charlie.id, name: "IWC", style: "Pilot", location: "Montreal", price: 50, img_url: "https://gothelist.com/media/catalog/product/cache/image/e9c3970ab036de70892d86c6d221abfe/imp/ort/content-media-product-sources-8db432eb-cddf-f5e3-d5dd-dcaecdf1cab5.jpg")
 ap = Watch.create(user_id: charlie.id, name: "Audemars Piguet", style: "Dress", location: "New York", price: 100, img_url: "https://gothelist.com/media/catalog/product/cache/image/e9c3970ab036de70892d86c6d221abfe/imp/ort/content-media-product-sources-bbe5a499-949d-42d8-86e2-155bfe039737.jpg")
+vc = Watch.create(user_id: alex.id, name: "Vacheron Constantin", style: "Dress", location: "Swiss", price: 100, img_url: "https://www.tissotwatches.com/media/catalog/product/cache/aaadd316e453df5b08f7f4246fad1a9c/T/1/T127.407.11.091.01_R_1.png")
+pp = Watch.create(user_id: bob.id, name: "Patek Phillippe", style: "Dress", location: "Swiss", price: 100, img_url: "https://www.tissotwatches.com/media/catalog/product/cache/aaadd316e453df5b08f7f4246fad1a9c/T/1/T137.407.11.041.00_R.png")
+tissot = Watch.create(user_id: bob.id, name: "Tissot", style: "Dress", location: "Swiss", price: 100, img_url: "https://www.tissotwatches.com/media/catalog/product/cache/aaadd316e453df5b08f7f4246fad1a9c/T/1/T116.617.11.057.01_R_1.png")
+tissot_2 = Watch.create(user_id: bob.id, name: "Tissot T Touch", style: "Dress", location: "Swiss", price: 100, img_url: "https://www.tissotwatches.com/media/catalog/product/cache/aaadd316e453df5b08f7f4246fad1a9c/T/1/T121.420.44.051.00_R_1.png")
 
 puts "Creating bookings..."
 booking1 = Booking.create(user_id: bob.id, watch_id: rolex.id, start_date: (Date.current + 1), end_date: (Date.current + 2), status: 0)
 booking2 = Booking.create(user_id: bob.id, watch_id: omega.id, start_date: (Date.current + 3), end_date: (Date.current + 4), status: 0)
 booking3 = Booking.create(user_id: bob.id, watch_id: casio.id, start_date: (Date.current + 5), end_date: (Date.current + 6), status: 0)
-booking4 = Booking.create(user_id: bob.id, watch_id: casio.id, start_date: (Date.current + 7), end_date: (Date.current + 8), status: 0)
-booking5 = Booking.create(user_id: bob.id, watch_id: casio.id, start_date: (Date.current + 9), end_date: (Date.current + 10), status: 0)
-booking6 = Booking.create(user_id: bob.id, watch_id: casio.id, start_date: (Date.current + 11), end_date: (Date.current + 12), status: 1)
-booking7 = Booking.create(user_id: alex.id, watch_id: iwc.id, start_date: (Date.current + 13), end_date: (Date.current + 14), status: 0)
-booking8 = Booking.create(user_id: alex.id, watch_id: ap.id, start_date: (Date.current + 15), end_date: (Date.current + 16), status: 1)
-booking9 = Booking.create(user_id: alex.id, watch_id: ap.id, start_date: (Date.current + 17), end_date: (Date.current + 18), status: 1)
-booking10 = Booking.create(user_id: alex.id, watch_id: ap.id, start_date: (Date.current + 19), end_date: (Date.current + 20), status: 1)
-booking11 = Booking.create(user_id: alex.id, watch_id: ap.id, start_date: (Date.current + 21), end_date: (Date.current + 22), status: 1)
-booking12 = Booking.create(user_id: alex.id, watch_id: ap.id, start_date: (Date.current + 23), end_date: (Date.current + 24), status: 2)
+booking4 = Booking.create(user_id: alex.id, watch_id: iwc.id, start_date: (Date.current + 13), end_date: (Date.current + 14), status: 0)
+booking5 = Booking.create(user_id: alex.id, watch_id: ap.id, start_date: (Date.current + 15), end_date: (Date.current + 16), status: 1)
 
 puts "Creating reviews..."
 seed1 = Review.create(comment: "What an amazing watch! I had a great time wearing it.", rating: 5, watch_id: omega.id)
