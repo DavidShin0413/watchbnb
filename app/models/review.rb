2afff8e15cfe12dcf13c7ecbd888fad68d953397
Joon Ho Shin <joonhoshin2021@gmail.com>
2021-11-19 13:09:23 -0500

    improved rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :watch
+  validates :comment, presence: true, length: { maximum: 255, too_long: "Maximum 255 characters allowed" }
+  validates :rating, presence: true, inclusion: { in: 1..5, message: "should be from 1 to 5" }
 end
